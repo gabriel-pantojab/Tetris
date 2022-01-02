7f09f879333464211db204b1d39d5667417be7e2
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2022-01-02 15:15:02 -0400

    revision

---
+++ game/RandomWallFactory.java
@@ -0,0 +1,30 @@
+package game;
+
+import Elements.*;
+import java.awt.Color;
+
+/**
+ * Write a description of class RandomWallFactory here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RandomWallFactory implements WallFactory{
+    public Wall createWall() {
+        int type    = random.nextInt(7)+1;
+        int column  = 2;
+        int row     = 27;
+        Color color = colors[random.nextInt(colors.length)];
+        
+        Wall wall;
+        if(type == 1) wall = new ShapeEle(row, column, color);
+        else if(type == 2) wall = new ShapeJ(row, column, color);
+        else if(type == 3) wall = new ShapeT(row, column, color);
+        else if(type == 4) wall = new ShapeZeta(row, column, color);
+        else if(type == 5) wall = new ShapeS(row, column, color);
+        else if(type == 6) wall = new ShapeRect(row, column, color);
+        else wall = new ShapeSquare(row, column, color);
+        
+        return wall;
+    }
+}
---
+++ game/WallFactory.java
@@ -0,0 +1,18 @@
+package game;
+import Elements.*;
+import java.util.Random;
+import java.awt.Color;
+
+/**
+ * Write a description of interface WallFactory here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public interface WallFactory {
+    public final Random random = new Random();
+    public final Color[] colors   = {new Color(0, 0, 200), new Color(0, 200, 0), new Color(128, 0, 255), new Color(255, 127, 39), 
+                                       new Color(200, 0, 0), Color.ORANGE, new Color(185, 122, 87), new Color(64, 128, 128),
+                                        new Color(251, 49, 201)};
+    public abstract Wall createWall();
+}
